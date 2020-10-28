d34c6eb34ffbb02cb4cf13ef981509e3944e0599
ibarzabal-jm <ibarzabalsallesjm@gmail.com>
2020-10-28 20:13:21 -0300

    Update AppRouter.js

--- maidapp/src/routers/AppRouter.js
+++ maidapp/src/routers/AppRouter.js
@@ -5,27 +5,52 @@ import {
     Route,
     Redirect
   } from "react-router-dom";
-  
+
+import {
+    Hidden,
+    makeStyles
+} from '@material-ui/core'
+
 import { Navbar } from '../components/ui/Navbar';
 import { HomeScreen } from '../components/ui/HomeScreen';
 import { AuthRouter } from './AuthRouter';
+import SideBar from '../components/ui/SideBar';
 
 
+const drawerWidth = 240;
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+  },
+  toolbar: theme.mixins.toolbar,
+  content: {
+    flexGrow: 1,
+    backgroundColor: theme.palette.background.default,
+    padding: theme.spacing(3),
+  },
+}));
 
 export const AppRouter = () => {
+
+    const classes = useStyles();
+
     return (
         <Router>
-            <div>
+            <div className={classes.root} >
                 <Navbar/>
-
-                <Switch>
-                    <Route path="/auth" component={AuthRouter} />
-
-                    <Route exact path="/" component={HomeScreen} />
-
-                    <Redirect to="/" />
-
-                </Switch>
+                <Hidden xsDown>
+                    <SideBar/>
+                </Hidden>
+                <div className={classes.content}>
+                    <div className={classes.toolbar}></div>
+                    Hola
+                        <Switch >
+                            <Route path="/auth" component={AuthRouter} />
+                            <Route exact path="/" component={HomeScreen} />
+                            <Redirect to="/" />
+                        </Switch>
+                    </div>
             </div>
         </Router>
     )
