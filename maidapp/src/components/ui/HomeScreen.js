34f31a9bd425c6122a426fd8f35385fc3ce39897
ibarzabal-jm <ibarzabalsallesjm@gmail.com>
2020-10-28 20:13:02 -0300

    Update HomeScreen.js

--- maidapp/src/components/ui/HomeScreen.js
+++ maidapp/src/components/ui/HomeScreen.js
@@ -1,9 +1,31 @@
 import React from 'react'
 
+
+import { Button } from '@material-ui/core'
+import  Hiden  from './Hiden'
+
+
+
 export const HomeScreen = () => {
+    
+    
+
     return (
         <div>
             <h1>Home</h1>
+            <Button variant="contained" color="primary">
+              Boton
+            </Button>
+            <Button disableElevation variant="contained" color="secondary">
+              Boton
+            </Button>
+            <Button variant="contained" color="primary" href='https://google.com' >
+              Boton
+            </Button>
+
+            <Hiden/>
+
+            
         </div>
     )
 }
