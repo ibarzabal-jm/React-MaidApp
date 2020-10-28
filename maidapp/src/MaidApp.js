7261182fedc137c85639ac68d0de8c8bc7f7d258
ibarzabal-jm <ibarzabalsallesjm@gmail.com>
2020-10-27 22:03:33 -0300

    Create MaidApp.js

---
+++ maidapp/src/MaidApp.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { AppRouter } from './routers/AppRouter'
+
+export const MaidApp = () => {
+    return (
+        <div>
+            <AppRouter/>
+        </div>
+    )
+}
