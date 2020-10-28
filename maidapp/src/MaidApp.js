eaea08ba317ec395602259fda8e8a74f3d7ad7a7
ibarzabal-jm <ibarzabalsallesjm@gmail.com>
2020-10-28 20:13:18 -0300

    Update MaidApp.js

--- maidapp/src/MaidApp.js
+++ maidapp/src/MaidApp.js
@@ -1,10 +1,15 @@
 import React from 'react'
+import { ThemeProvider } from '@material-ui/core/styles'
+import theme from './styles/themeConfig'
 import { AppRouter } from './routers/AppRouter'
+
 
 export const MaidApp = () => {
     return (
         <div>
-            <AppRouter/>
+            <ThemeProvider theme={theme}>
+                <AppRouter/>
+            </ThemeProvider>
         </div>
     )
 }
