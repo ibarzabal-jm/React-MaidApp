731dcd7577505db05d061c3e6d184b42decce1c8
ibarzabal-jm <ibarzabalsallesjm@gmail.com>
2020-10-29 23:05:10 -0300

    Create Copyright.js

---
+++ maidapp/src/components/ui/Copyright.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import {Link} from 'react-router-dom'
+import Typography from '@material-ui/core/Typography'
+
+
+
+
+export const Copyright = () => {
+    return (
+        <Typography variant="body2" color="textSecondary" align="center">
+          {'Copyright © '}
+          <Link color="inherit" to="/">
+            iULY
+          </Link>{' '}
+          {new Date().getFullYear()}
+          {'.'}
+        </Typography>
+    );
+}
+
+
