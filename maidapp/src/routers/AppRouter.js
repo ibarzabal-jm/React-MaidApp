9159a263c4ebbddfcffe6f1130063300ad849c00
ibarzabal-jm <ibarzabalsallesjm@gmail.com>
2020-11-19 14:29:53 -0300

    Update AppRouter.js

--- maidapp/src/routers/AppRouter.js
+++ maidapp/src/routers/AppRouter.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React from 'react'
 import {
     BrowserRouter as Router,
     Switch,
@@ -6,63 +6,23 @@ import {
     Redirect
   } from "react-router-dom";
 
-import {
-    Hidden,
-    makeStyles
-} from '@material-ui/core'
-
 import { Navbar } from '../components/ui/Navbar';
-import { HomeScreen } from '../components/ui/HomeScreen';
+import { HomeScreen } from '../components/containers/HomeScreen';
 import { AuthRouter } from './AuthRouter';
-import SideBar from '../components/ui/SideBar';
 
-
-
-const useStyles = makeStyles((theme) => ({
-  root: {
-    display: 'flex',
-  },
-  toolbar: theme.mixins.toolbar,
-  content: {
-    flexGrow: 1,
-    backgroundColor: theme.palette.background.default,
-    padding: theme.spacing(3),
-  },
-}));
 
 export const AppRouter = () => {
 
-    const classes = useStyles();
-    const [open, setOpen] = useState(false);
-
-    const handleToggle = () =>{
-        setOpen(!open);
-    }
-
     return (
         <Router>
-            <div className={classes.root} >
-                <Navbar handleToggle={handleToggle}/>
-                <Hidden mdDown>
-                    <SideBar variant="permanent"
-                             open={true}
-                    />
-                </Hidden>
-                <Hidden smUp>
-                    <SideBar variant="temporary"
-                             open={open}
-                             onClose={handleToggle}
-                    />
-                </Hidden>
-
-                <div className={classes.content}>
-                    <div className={classes.toolbar}></div>
-                        <Switch >
-                            <Route path="/auth" component={AuthRouter} />
-                            <Route exact path="/" component={HomeScreen} />
-                            <Redirect to="/" />
-                        </Switch>
-                    </div>
+            <div>
+                <Navbar />
+           
+                <Switch >
+                    <Route path="/auth" component={AuthRouter} />
+                    <Route exact path="/" component={HomeScreen} />
+                    <Redirect to="/" />
+                </Switch>
             </div>
         </Router>
     )
