32a7e57e36cd38a418ea77f643dcd7e99fce96d6
ibarzabal-jm <ibarzabalsallesjm@gmail.com>
2020-11-19 14:30:02 -0300

    Update Navbar.js

--- maidapp/src/components/ui/Navbar.js
+++ maidapp/src/components/ui/Navbar.js
@@ -1,58 +1,72 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-
 import { AppBar, Button, IconButton, makeStyles, Toolbar } from '@material-ui/core';
+import NavbarScroll from './NavbarScroll'
 import MenuIcon from '@material-ui/icons/Menu'
 import logo from '../../images/logo-iuly.png';
 
-const drawerWidth = 240;
-
 const useStyle = makeStyles( theme => ({
-    appBar: {
-        [theme.breakpoints.up('sm')]:{
-            width: `calc(100% - ${drawerWidth}px)`,
-            marginLeft: drawerWidth,    
-        }
-    },
+ 
     menuButton:{
         flexGrow: 1,
         marginRight: theme.spacing(2),
         [theme.breakpoints.up('sm')]:{
             display:'none',
         },
+        "&:hover": {
+            color: theme.palette.warning.main
+        }
     },
     navButton:{
-        marginRight: theme.spacing(1)
+        marginRight: theme.spacing(1),
+        [theme.breakpoints.down('xs')]:{
+            display:'none',
+        },
+        "&:hover": {
+            color: theme.palette.warning.main
+        }
     },
     title:{
         flexGrow: 1,
     },
 }))
 
-export const Navbar = ({handleToggle}) => {
+export const Navbar = () => {
 
     const classes = useStyle();
 
     return (
-        <AppBar position="fixed" color="primary" className={classes.appBar} >
-            <Toolbar>
-                
-                <IconButton className={classes.menuButton} color="inherit" aria-label="menu" onClick={handleToggle}>
-                    <MenuIcon />
-                </IconButton>
+        <NavbarScroll>
+            <AppBar position="fixed">
+                <Toolbar>
+                    <IconButton className={classes.menuButton} color="inherit" aria-label="menu" >
+                        <MenuIcon />
+                    </IconButton>
 
-                <Link to="/" className={classes.title}>
-                            <img alt="logo-iULY" style={{ width:"100px", height : "40px", marginRight : "0.5rem" }} src={logo}></img>
-                </Link>
+                    <Link to="/" className={classes.title}>
+                                <img alt="logo-iULY" style={{ width:"100px", height : "40px", marginRight : "0.5rem" }} src={logo}></img>
+                    </Link>
 
-                <Button variant="contained" color="primary" className={ classes.navButton }>
-                    Ingresar
-                </Button>
-                <Button variant="contained" color="primary" className={ classes.navButton }>
-                    Registro
-                </Button>
-            </Toolbar>
-        </AppBar>
+                    <Button variant="text"
+                        color="inherit"
+                        className={ classes.navButton }
+                        component={Link}
+                        to="/auth/login"
+                    >
+                        Ingresar
+                    </Button>
+
+                    <Button variant="text"
+                        color="inherit"
+                        className={ classes.navButton }
+                        component={Link}
+                        to="/register"
+                    >
+                        Registro
+                    </Button>
+
+                </Toolbar>
+            </AppBar>
+        </NavbarScroll>
     )
 }
-
