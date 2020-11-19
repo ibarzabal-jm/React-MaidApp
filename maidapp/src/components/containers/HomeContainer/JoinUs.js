43f1d5c78eac4eb49c4994d1816e919c4e21eab5
ibarzabal-jm <ibarzabalsallesjm@gmail.com>
2020-11-19 14:29:24 -0300

    Create JoinUs.js

---
+++ maidapp/src/components/containers/HomeContainer/JoinUs.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const JoinUs = () => {
+    return (
+        <div>
+            <h1>
+                PorqueUnirse
+            </h1>
+        </div>
+    )
+}
+
+export default JoinUs
