beaeff8522aac5d09314a6ae6c15001d2731364f
ibarzabal-jm <ibarzabalsallesjm@gmail.com>
2020-11-19 14:29:48 -0300

    Update themeConfig.js

--- maidapp/src/styles/themeConfig.js
+++ maidapp/src/styles/themeConfig.js
@@ -2,7 +2,23 @@ import {createMuiTheme} from '@material-ui/core/styles'
 
 const theme = createMuiTheme({
     palette:{
-    }
+        primary:{
+            main: '#26C6DA'
+        },
+        secondary:{
+            main:'#D93B8B'
+        },
+        warning:{
+            main: '#ffd54f'
+        }
+    },
+    typography: {
+        fontFamily: [
+          'Poppins',
+          'sans-serif',
+          '-apple-system',
+        ].join(','),
+    },
 })
 
 export default theme;
