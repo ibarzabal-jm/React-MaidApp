1f7dbfe1571813121cd06e86f245a9f3ec8c9bbe
ibarzabal-jm <ibarzabalsallesjm@gmail.com>
2020-11-22 20:08:16 -0300

    Create IconCard.js

---
+++ maidapp/src/components/containers/HomeContainer/IconCard.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Card from '@material-ui/core/Card'
+
+export const IconCard = ({icon, titulo, texto}) => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
