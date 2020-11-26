502f665e43f515b3b003a6763282b92038dc4759
ibarzabal-jm <ibarzabalsallesjm@gmail.com>
2020-11-26 17:21:24 -0300

    Update Introduccion.js

--- maidapp/src/components/containers/HomeContainer/Introduccion.js
+++ maidapp/src/components/containers/HomeContainer/Introduccion.js
@@ -9,6 +9,13 @@ export const Introduccion = () => {
             </Grid>
             <Grid item xs={12}>
                 <p>
+                    iULY es una pagina web creada por mí, Juan Manuel Ibarzabal Salles, cuyo objetivo es demostrar lo que he aprendido a lo largo de este 2020.
+                    Mi objetivo era hacer una especie de Rappi/Pedidos ya y otras compañias similares de mucamas. 
+                    
+                    Tratando de hacerlo mucho más creíble, decidí consultar a varias trabajadoras cercanas a mi circulo intimo, donde me pareció conveniente limitar su uso a únicamente Mujeres. 
+                    ¿Por qué mujeres? Simplemente porque hay una poca taza de violencia y una nula de abuso sexual. Las mucamas, (por lo menos mi suegra prefiere que le digan mucama antes que empleada doméstica)
+                    generalmente lidian con niños, gente anciana, lo último que querría yo poniendome en el lugar de una compañia claramente es lidiar con violadores.
+                    
                     iULY es una app cuyo objetivo es hacer más fácil, más rápida y segura el servicio de Limpieza de un hogar particular u oficina.
                 </p>
             </Grid>
