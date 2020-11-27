5198af15dc56922110b18a2e2d0148ddf2ce178e
ibarzabal-jm <ibarzabalsallesjm@gmail.com>
2020-11-27 17:13:37 -0300

    Update MaidList.js

--- maidapp/src/components/maid/MaidList.js
+++ maidapp/src/components/maid/MaidList.js
@@ -1,10 +1,70 @@
 import React from 'react'
+import MaidCard from './MaidCard'
+import { Grid, Typography } from '@material-ui/core'
+import { makeStyles } from '@material-ui/core';
 
+
+
+
+const useStyles = makeStyles( (theme)=> ({
+    root: {
+        backgroundColor:'pink',
+        
+    }  
+}) )
+
+const mucamas = [
+    {
+        id: 1,
+        nombre: 'rita',
+        nacionalidad: 'argentina',
+        img: 'https://placekitten.com/408/287',
+        edad: 30,
+        modalidad: 'Cama Adentro',
+        alta: 'Mayo 2020',
+        update: new Date(),
+    },
+    {
+        id: 2,
+        nombre: 'fasfas',
+        nacionalidad: 'argentina',
+        modalidad: 'Cama Adentro',
+        edad: 30,
+        alta: 'Abril 2020',
+        update: new Date(),
+    },
+
+]
 export const MaidList = () => {
+    const classes = useStyles();
+
     return (
-        <div>
-            <h1>MaidList</h1>
-            <h2>Cambio</h2>
-        </div>
+        <Grid container
+            alignContent="center"
+            alignItems="center"
+            justify="center"
+            direction="row"
+            spacing={4}
+        
+            className={classes.root}
+        >
+            <Grid item xs={12}>
+                
+                <Typography variant="h3" align="center" > Nuestras iULY's</Typography>
+                
+            </Grid>
+
+            {
+                mucamas.map( (mucama) =>(
+
+                    <Grid item xs={4} >
+                        <MaidCard
+                            maid={mucama}
+                        />
+                    </Grid>
+                )
+                )
+            }
+        </Grid>
     )
 }
