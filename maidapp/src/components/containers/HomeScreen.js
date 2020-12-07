1737bd3412f656b47cfdf22946881109359bb94a
ibarzabal-jm <ibarzabalsallesjm@gmail.com>
2020-12-06 21:04:18 -0300

    Update HomeScreen.js

--- maidapp/src/components/containers/HomeScreen.js
+++ maidapp/src/components/containers/HomeScreen.js
@@ -3,6 +3,7 @@ import Grid from '@material-ui/core/Grid'
 import Portada from './HomeContainer/Portada'
 import JoinUs  from './HomeContainer/JoinUs'
 import Contratanos from './HomeContainer/Contratanos'
+import Introduccion  from './HomeContainer/Introduccion'
 
 
 export const HomeScreen = () => {
@@ -10,6 +11,7 @@ export const HomeScreen = () => {
     return (
         <Grid container >
             <Portada/>
+            <Introduccion />
             <JoinUs />
             <Contratanos/>
         </Grid>
