ea95e3a99d5ae4c61de18390326010e1cbf86ef4
ibarzabal-jm <ibarzabalsallesjm@gmail.com>
2020-12-19 15:22:36 -0300

    Update MaidList.js

--- maidapp/src/components/maid/MaidList.js
+++ maidapp/src/components/maid/MaidList.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import MaidCard from './MaidCard'
-import { Grid, Typography } from '@material-ui/core'
+import { Grid, Typography, Toolbar } from '@material-ui/core'
 import { makeStyles } from '@material-ui/core';
 
 
@@ -22,6 +22,7 @@ const mucamas = [
         edad: 30,
         modalidad: 'Cama Adentro',
         alta: 'Mayo 2020',
+        residencia:'Caballito',
         update: new Date(),
     },
     {
@@ -32,6 +33,7 @@ const mucamas = [
         img: 'https://placekitten.com/408/287',
         edad: 30,
         alta: 'Abril 2020',
+        residencia:'Caballito',
         update: new Date(),
     },
 
@@ -49,6 +51,7 @@ export const MaidList = () => {
                 direction="row"
                 spacing={4}
             >
+                <Toolbar/>
                 <Grid item xs={12}>
                     
                     <Typography variant="h3" align="center" > Nuestras iULY's</Typography>
@@ -57,7 +60,6 @@ export const MaidList = () => {
     
                 {
                     mucamas.map( (mucama) =>(
-    
                         <Grid item xs={4} >
                             <MaidCard
                                 maid={mucama}
