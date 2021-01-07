b01ba7472fa282d6b771d23b91cbb35b9947db04
Juan Manuel Ibarzabal Salles <ibarzabalsallesjm@gmail.com>
2021-01-07 16:42:26 -0300

    Update AppRouter.js

--- maidapp/src/routers/AppRouter.js
+++ maidapp/src/routers/AppRouter.js
@@ -1,31 +1,31 @@
-import React from 'react'
+import React from "react";
 import {
-    BrowserRouter as Router,
-    Switch,
-    Route,
-    Redirect
-  } from "react-router-dom";
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
 
-import { Navbar } from '../components/ui/Navbar';
-import { HomeScreen } from '../components/containers/HomeScreen';
-import { AuthRouter } from './AuthRouter';
-import { MaidList } from '../components/maid/MaidList';
-
+import { Navbar } from "../components/ui/Navbar";
+import { HomeScreen } from "../components/containers/HomeScreen";
+import { AuthRouter } from "./AuthRouter";
+import { MaidList } from "../components/maid/MaidList";
+import Footer from "../components/ui/Footer";
 
 export const AppRouter = () => {
+  return (
+    <Router>
+      <div>
+        <Navbar />
 
-    return (
-        <Router>
-            <div>
-                <Navbar />
-           
-                <Switch >
-                    <Route path="/auth" component={AuthRouter} />
-                    <Route path="/maid" component={MaidList} />
-                    <Route exact path="/" component={HomeScreen} />
-                    <Redirect to="/" />
-                </Switch>
-            </div>
-        </Router>
-    )
-}
+        <Switch>
+          <Route path="/auth" component={AuthRouter} />
+          <Route path="/maid" component={MaidList} />
+          <Route exact path="/" component={HomeScreen} />
+          <Redirect to="/" />
+        </Switch>
+        <Footer />
+      </div>
+    </Router>
+  );
+};
